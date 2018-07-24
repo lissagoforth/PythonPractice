ead251bcdc68a021742cc3ef2c822b1480a07527
Lissa Goforth <lissa.goforth@bti.tv>
2018-07-24 12:40:56 -0500

    uncommitted save only comments added

--- Operators/hello.py
+++ Operators/hello.py
@@ -11,3 +11,29 @@ print(37 % 3)  # percent sign is a modulus operator
 print('What is your name?')
 name = input()  # read a single line and store it in the variable "name"
 print('Hi ' + name + '!')
+
+a = input() #interprets user input as a string
+b = input()
+s = a + b
+print(s) #this will just concat user input. If you are expecting an int you must do the following...
+
+a = int(input())
+b = int(input())
+s = a + b
+print(s)
+
+#   Calculate the Simple Interest
+#
+#   The formula for Simple Interest is
+#   si = principal * interest rate * number of years
+#   where si is the simple interest
+
+p = 18819.99    # principal
+r = 0.05        # rate of interest
+t = 2           # years
+
+si = p * r * t
+print("Simple interest at the end of 2 years $", format(si, "0.2f")) # format(value, format-specifier) 0 is width (total space) is automaitcally increased as needed. 2 is to what decimal point. If you want scientific notation you can replace the 'f' with 'e' or 'E'. Add ',' before precision to add commas to make large numbers more readable
+print(format(98813343817.7129, ",.2f"))
+98,813,343,817.71 
+#   Type codes d, b, o, x can be used to format in decimal, binary, octal and hexadecimal respectively
